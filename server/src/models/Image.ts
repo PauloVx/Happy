a3a61707180860ec55503ad68f164d27851ab039
Paulo <paulituvitu@hotmail.com>
2020-10-13 20:42:00 -0300

    Finished api

---
+++ server/src/models/Image.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+import { Orphanage } from './Orphanage';
+
+@Entity('images')
+export class Image {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  path: string;
+
+  @ManyToOne(() => Orphanage, orphanage => orphanage.images)
+  @JoinColumn({ name: 'orphanage_id' })
+  orphanage: Orphanage;
+}
