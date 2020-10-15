689f8b1b3a357c197f888a6cf38d7a0820e55aec
Paulo <paulituvitu@hotmail.com>
2020-10-15 15:15:51 -0300

    Simple map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, StyleSheet, Text, View, Image } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: -22.8507248,
+          longitude: -43.2652029,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8
+          }}
+          coordinate={{
+            latitude: -22.8507248,
+            longitude: -43.2652029
+          }}
+        >
+          <Callout tooltip>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Orph Name</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -14,8 +45,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').height,
+  },
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+  calloutText: {
+    color: '#0089A5',
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
 });
